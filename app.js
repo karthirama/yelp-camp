611dacf2460043d1d8e3f7108ccf4fc25ae8f6e6
ramachandran <karthirama429@gmail.com>
2023-05-05 22:21:54 +0530

    Crud Opeartions Added

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./model/campground')
+const methodOverride = require('method-override')
+const Campground = require('./model/campground');
+const campground = require('./model/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser: true,
@@ -18,6 +20,8 @@ db.once("open",() => {
 
 app.set("view engine",'ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
 
 app.get("/",(req,res)=>{
     res.render('home')
@@ -32,6 +36,39 @@ catch(error){
     console.log("Error occured")
 }
 });
+
+app.get("/campgrounds/new",(req,res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds',async(req,res) => {
+    const newCamp = new Campground(req.body.campground);
+    await newCamp.save();
+    res.redirect(`/campgrounds/${newCamp._id}`);
+});
+
+app.get('/campgrounds/:id',async (req,res) => {
+   const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{campground});
+})
+
+app.get('/campgrounds/:id/edit',async (req,res) => {
+    const campground = await Campground.findById(req.params.id);
+     res.render('campgrounds/edit',{campground});
+ })
+
+ app.put('/campgrounds/:id',async (req,res) => {
+    const id = req.params.id;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground.id}`);
+ })
+
+ app.delete('/campgrounds/:id',async (req,res) => {
+    const id = req.params.id;
+    const campground = await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds/');
+ });
+
 
 app.listen(3000,() => {
     console.log("Serving on port 3000");
